45698b77e7c41e80150e9eef62b446babb2e4df2
ilipovcev <ilya.lipovcev@bk.ru>
2021-03-26 19:58:01 +0300

    Игра включается после нажатия на кнопку Host Game.

--- StartMenu/Menu.gd
+++ StartMenu/Menu.gd
@@ -1,11 +1,24 @@
 extends Control
 
-
-
 func _ready():
 	pass
 
 
 func _on_HostButton_pressed():
 	Server.connectServer()
+	get_tree().change_scene("res://Game.tscn")
+	
+
+func _on_JoinButton_pressed():
+	var inputIP = get_node("NinePatchRect/VBoxContainer/InputIP").get_text()
+	var inputPort = get_node("NinePatchRect/VBoxContainer/InputPort").get_text()
+
+	if inputIP == "":
+		print("Invalid value")
+		return
+		
+	if inputPort == "":
+		print("Invalid value")
+		return
+
 	
